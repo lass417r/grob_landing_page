3a522455cfa41152472d7437eecc1b1e91ce99b3
”Lasse <lass417r@stud.kea.dk>
2022-03-18 14:26:46 +0100

    lykkehjul

---
+++ lykkehjul.js
@@ -0,0 +1,18 @@
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+  document.querySelector("#spin_knap").addEventListener("click", lykkehjulSpin);
+  document.querySelector("#email").classList.remove("skjult");
+}
+
+function lykkehjulSpin() {
+  document.querySelector("#email").classList.add("skjult");
+  var x = 1024;
+  var b = 9999;
+  var dgre = Math.floor(Math.random() * (x - b)) + b;
+  document.querySelector("#lykkehjul_skive").style.transform =
+    "rotate(" + dgre + "deg)";
+  document
+    .querySelector("#lykkehjul_skive")
+    .addEventListener("animationend", start);
+}
