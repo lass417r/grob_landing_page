93ab1e951dca2703001550ca818111f754caa4dc
”Lasse <lass417r@stud.kea.dk>
2022-03-21 10:48:00 +0100

    pusher

--- lykkehjul.js
+++ lykkehjul.js
@@ -2,11 +2,11 @@ window.addEventListener("DOMContentLoaded", start);
 
 function start() {
   document.querySelector("#spin_knap").addEventListener("click", lykkehjulSpin);
-  document.querySelector("#email").classList.remove("skjult");
+  // document.querySelector("#email").classList.remove("skjult");
 }
 
 function lykkehjulSpin() {
-  document.querySelector("#email").classList.add("skjult");
+  // document.querySelector("#email").classList.add("skjult");
   var x = 1024;
   var b = 9999;
   var dgre = Math.floor(Math.random() * (x - b)) + b;
