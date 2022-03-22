0dd68c24fbe62fe0095acee36652228e0f0e1004
”Lasse <lass417r@stud.kea.dk>
2022-03-22 11:59:30 +0100

    grain og splash fix

--- js/lykkehjul.js
+++ js/lykkehjul.js
@@ -1,17 +1,29 @@
+// Lytter efter om DOM'en er loadet og starter functionen start
 window.addEventListener("DOMContentLoaded", start);
 
+// Her lytter vi efter klik på "Spin" knappen
 function start() {
   document.querySelector("#spin_knap").addEventListener("click", lykkehjulSpin);
-  // document.querySelector("#email").classList.remove("skjult");
 }
 
+// I denne function får vi lykkehjulet til at spinne med en random grad.
 function lykkehjulSpin() {
-  // document.querySelector("#email").classList.add("skjult");
+  // Her fjerner vi eventListener så man ikke kan trykke imens animationen er igang.
+  document
+    .querySelector("#spin_knap")
+    .removeEventListener("click", lykkehjulSpin);
+  // Her har vi lavet nogle variabler med forskellige tal.
   var x = 1024;
   var b = 9999;
+  // Her laver vi en variable ved hjælp af Math random og de tidligere variabler.
   var dgre = Math.floor(Math.random() * (x - b)) + b;
+  // Her vælger vi lykkehjulet og får det til at rotere det antal grader
+  // linjen over har udregnet.
   document.querySelector("#lykkehjul_skive").style.transform =
     "rotate(" + dgre + "deg)";
+  // Her tilføjer vi eventListener igen.
+  document.querySelector("#spin_knap").addEventListener("click", lykkehjulSpin);
+  // Her starter vi scriptet forfra og gør klar til endnu et spin.
   document
     .querySelector("#lykkehjul_skive")
     .addEventListener("animationend", start);
